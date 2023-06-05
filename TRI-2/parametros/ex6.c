9e99986a119625a13de8e7b6796c40fa772532d1
VitorTavaresO <vitortavares.o55@gmail.com>
2023-06-05 10:44:26 -0300

    Ex6

--- TRI-2/parametros/ex6.c
+++ TRI-2/parametros/ex6.c
@@ -1,1 +1,15 @@
-void converteHora(int total segundos, int *hora, int *min, int *seg)
+#include <stdio.h>
+void convertSegToHour(int totalSeconds, int *hour, int *min, int *sec){
+    *hour = totalSeconds / 3600;
+    *min = (totalSeconds % 3600) / 60;
+    *sec = (totalSeconds % 3600) % 60;
+}
+
+int main(){
+    int totalSeconds, hour, min, sec;
+    printf("Digite o total de segundos: ");
+    scanf("%d", &totalSeconds);
+    convertSegToHour(totalSeconds, &hour, &min, &sec);
+    printf("%d:%d:%d", hour, min, sec);
+    return 0;
+}
