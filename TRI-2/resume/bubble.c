a9b6f4cf1e8382c6268b89d5f75866a3a4927104
VitorTavaresO <vitortavares.o55@gmail.com>
2023-06-26 14:38:09 -0300

    bubble

---
+++ TRI-2/resume/bubble.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int numero[4] = {4, 2, 1, 3};
+void main(void)
+{
+    int i, j, aux, n;
+    for (i = 3; i > 0; i--)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (numero[j] > numero[j + 1])
+            {
+                aux = numero[j + 1];
+                numero[j + 1] = numero[j];
+                numero[j] = aux;
+            }
+        }
+    }
+
+    for (i = 0; i <= 3; i++)
+    {
+        printf("%d", numero[i]);
+    }
+}
