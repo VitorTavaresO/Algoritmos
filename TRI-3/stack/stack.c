1bc9ce7dc5e706b6b143496a5c88a346dee4fd9e
VitorTavaresO <vitortavares.o55@gmail.com>
2023-10-03 20:05:04 -0300

    Stack

--- TRI-3/stack/stack.c
+++ TRI-3/stack/stack.c
@@ -2,3 +2,74 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <time.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+
+typedef struct stack
+{
+    Node *top;
+} Stack;
+
+void init(Stack *stack)
+{
+    stack->top = NULL;
+}
+
+int is_empty(Stack *stack)
+{
+    return stack->top == NULL;
+}
+
+void push(Stack *stack, int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+
+int pop(Stack *stack)
+{
+    if (is_empty(stack))
+    {
+        printf("Pilha Vazia.\n");
+        return -1;
+    }
+    Node *temp = stack->top;
+    int data = temp->data;
+    stack->top = (stack->top)->next;
+    free(temp);
+    return data;
+}
+
+void printStack(Stack *stack)
+{
+    Node *temp = stack->top;
+    printf("[");
+    while (temp)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("]\n");
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL));
+
+    Stack stack;
+    init(&stack);
+
+    int data = pop(&stack);
+    printStack(&stack);
+    printf("Data: %d\n", data);
+    printStack(&stack);
+
+    return 0;
+}
